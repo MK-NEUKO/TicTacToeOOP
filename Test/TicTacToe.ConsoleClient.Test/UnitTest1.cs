e9469974e2c8e7617d21870f671eb54d6730e8cb
Michael Koch <m.koch@know-how-akademie.de>
2020-12-01 07:13:39 +0100

    Testprojekt für den ConsoleClient eingerichtet.

--- Test/TicTacToe.ConsoleClient.Test/UnitTest1.cs
+++ Test/TicTacToe.ConsoleClient.Test/UnitTest1.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace TicTacToe.Core.UnitTest
+namespace TicTacToe.ConsoleClient.Test
 {
     [TestClass]
     public class UnitTest1
---
+++ Test/TicTacToe.Core.Test/GameBoard_Test.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicTacToe.Core.Test
+{
+    [TestClass]
+    public class GameBoard_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
