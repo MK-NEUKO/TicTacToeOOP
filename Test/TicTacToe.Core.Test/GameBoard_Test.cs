95c4b7738fdc56b871577e19d9e98f430f8ff22b
Michael Koch <m.koch@know-how-akademie.de>
2020-12-05 22:52:47 +0100

    Erste Testmethode für die Gewinnerüberprüfung geschrieben.

--- Test/TicTacToe.Core.Test/GameBoard_Test.cs
+++ Test/TicTacToe.Core.Test/GameBoard_Test.cs
@@ -1,13 +1,37 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NEUKO.TicTacToe.Core;
+using System.Collections.Generic;
 
 namespace TicTacToe.Core.Test
 {
     [TestClass]
     public class GameBoard_Test
     {
+        
+
         [TestMethod]
-        public void TestMethod1()
+        public void WinConstellationOne_Test()
         {
+            List<GameBoardArea> boardAreaList = new List<GameBoardArea>();           
+            for (int areaID = 0; areaID < 9; areaID++)
+            {
+                boardAreaList.Add(new GameBoardArea(areaID));
+            }           
+            GameBoard board = new GameBoard(boardAreaList);
+
+            boardAreaList[0].Area = "X";
+            boardAreaList[1].Area = "X";
+            boardAreaList[2].Area = "X";
+            boardAreaList[3].Area = " ";
+            boardAreaList[4].Area = " ";
+            boardAreaList[5].Area = " ";
+            boardAreaList[6].Area = " ";
+            boardAreaList[7].Area = " ";
+            boardAreaList[8].Area = " ";
+
+            board.CheckGameBoardState();
+
+            Assert.IsTrue(board.PlayerXIsWinner);
         }
     }
 }
