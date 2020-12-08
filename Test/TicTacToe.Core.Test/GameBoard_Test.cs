b222c966c48e3cbad02ab9883e1d72723b537348
Michael Koch <m.koch@know-how-akademie.de>
2020-12-08 02:02:36 +0100

    Datengetriebene Testmethode für die Gewinnerermittlung und Unentschieden erstellt.

--- Test/TicTacToe.Core.Test/GameBoard_Test.cs
+++ Test/TicTacToe.Core.Test/GameBoard_Test.cs
@@ -1,13 +1,21 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NEUKO.TicTacToe.Core;
+using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace TicTacToe.Core.Test
 {
     [TestClass]
     public class GameBoard_Test
     {
-        
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
 
         [TestMethod]
         public void WinConstellationOne_Test()
@@ -32,6 +40,46 @@ namespace TicTacToe.Core.Test
             board.CheckGameBoardState();
 
             Assert.IsTrue(board.PlayerXIsWinner);
+            TestContext.WriteLine(TestContext.FullyQualifiedTestClassName);
+            TestContext.WriteLine(TestContext.TestName);
+        }
+
+        [DataTestMethod]
+        [DataRow(new string[9]{ "X", "X", "O",
+                                " ", "X", "O",
+                                "O", " ", "X" })]
+
+        [DataRow(new string[9]{ "O", "X", "X",
+                                " ", "X", "O",
+                                "X", " ", "O" })]
+        public void CheckWinConstallation_GameBoard(string[] testGameBoard)
+        {
+            List<GameBoardArea> boardAreaList = new List<GameBoardArea>();
+            for (int areaID = 0; areaID < 9; areaID++)
+            {
+                boardAreaList.Add(new GameBoardArea(areaID));
+            }
+            GameBoard board = new GameBoard(boardAreaList);
+
+            for (int index = 0; index < testGameBoard.Length; index++)
+            {
+                boardAreaList[index].Area = testGameBoard[index];
+            }
+            
+            board.CheckGameBoardState();
+
+            Assert.IsTrue(board.PlayerXIsWinner);
+            Assert.IsFalse(board.PlayerOIsWinner);
+            Assert.IsFalse(board.GameIsTie);
+            TestContext.WriteLine($"{testGameBoard[0]}|{testGameBoard[1]}|{testGameBoard[2]}");
+            TestContext.WriteLine($"{testGameBoard[3]}|{testGameBoard[4]}|{testGameBoard[5]}");
+            TestContext.WriteLine($"{testGameBoard[6]}|{testGameBoard[7]}|{testGameBoard[8]}");
+        }
+
+        [TestCleanup] 
+        public void TestCleanUp()
+        {
+            TestContext.WriteLine(TestContext.CurrentTestOutcome.ToString());
         }
     }
 }
