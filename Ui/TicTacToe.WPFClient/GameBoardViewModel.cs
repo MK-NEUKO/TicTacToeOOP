3714289a3051b441934bfc52cb0e708b7bfd1176
Michael Koch <m.koch@know-how-akademie.de>
2021-02-18 07:39:33 +0100

    GameBoard als User-Control erstellt und in das MainWindow eingefügt. Anlegen der Klassen und Interfaces für das GameBoard.

---
+++ Ui/TicTacToe.WPFClient/GameBoardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using NEUKO.TicTacToe.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEUKO.TicTacToe.WPFClient
+{
+    public class GameBoardViewModel : IGameBoardViewModel
+    {
+        public IReadOnlyList<GameBoardArea> GameBoardAreaList => throw new NotImplementedException();
+    }
+}
---
+++ Ui/TicTacToe.WPFClient/SampleData/GameBoardCommandDummy.cs
@@ -0,0 +1,10 @@
+﻿namespace NEUKO.TicTacToe.WPFClient.SampleData
+{
+    public class GameBoardCommandDummy
+    {
+        public int RowIndex { get; set; }
+        public int ColumnIndex { get; set; }
+        public int AreaID { get; set; }
+
+    }
+}
