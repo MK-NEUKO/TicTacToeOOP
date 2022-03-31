9c75ea688814c88238b8d8b701c34b3fe89da972
MK-NEUKO <m.koch@know-how-akademie.de>
2022-03-31 11:47:06 +0200

    ConsoleClient entfernt

--- Ui/TicTacToe.ConsoleClient/DisplayView.cs
+++ Ui/TicTacToe.ConsoleClient/DisplayView.cs
@@ -1,4 +1,5 @@
-﻿using MichaelKoch.TicTacToe.Logik.TicTacToeCore;
+﻿using MichaelKoch.TicTacToe.CrossCutting.DataClasses;
+using MichaelKoch.TicTacToe.Logik.TicTacToeCore;
 using MichaelKoch.TicTacToe.Logik.TicTacToeCore.Contract;
 using System;
 using System.Collections.Generic;
