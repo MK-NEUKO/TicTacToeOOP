aab157789cf246c13c9fcf9e56f5c05ea8727156
Michael Koch <m.koch@neuko-devment.com>
2024-06-13 11:10:16 +0200

    Major architecture and logging overhaul
    - Removed CommunityToolkit.Mvvm and related ViewModel code, indicating a shift in architectural approach.
    - Deleted specific ViewModel classes/interfaces tied to game logic, suggesting a game architecture redesign.
    - Introduced logging and configuration management in App.xaml.cs using Serilog and Microsoft.Extensions.Configuration, enhancing debugging capabilities.
    - Updated DesktopWPF.csproj with new Serilog package references, upgrading the logging framework.
    - Added configuration files (appsettings.json, appsettings.Development.json, appsettings.Production.json) for better application settings management.
    - Configured Serilog in new configuration files for flexible logging, including console and file outputs.
    - Implemented environment-specific configurations with appsettings.Development.json, appsettings.Production.json, and ASPNETCORE_ENVIRONMENT variable, allowing for distinct development and production settings.

--- samples/desktop/DesktopWPF/App.xaml.cs
+++ samples/desktop/DesktopWPF/App.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
 using Serilog;
 using TicTacToe.Application;
 using TicTacToe.Infrastructure;
@@ -16,20 +20,66 @@ namespace MichaelKoch.TicTacToe.Samples.DesktopWPF
     {
         public static IHost? AppHost { get; private set; }
 
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        private static extern bool AllocConsole();
+
+
         public App()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+            var configuration = BuildAppConfigurationRoot(environmentName);
+            var openConsoleInDebugMode = configuration.GetValue<bool>("ApplicationSettings:OpenConsoleInDebugMode");
+
+            CreateLoggerSerilog(configuration);
+
+            var builder = AddRequiredAppServices();
+            AppHost = builder.Build();
+
+
+#if DEBUG
+            if (openConsoleInDebugMode)
+            {
+                AllocConsole();
+                Log.Logger.Information("Console opened: Debug configuration / Development mode");
+            }
+#endif
+        }
+
+        private static IHostBuilder AddRequiredAppServices()
         {
             var builder = Host.CreateDefaultBuilder();
             builder.ConfigureServices(services =>
             {
+                services.AddSerilog();
                 services.AddSingleton<MainWindow>();
 
                 services.AddTicTacToeApplication();
                 services.AddTicTacToePresentation();
                 services.AddTicTacToeInfrastructure();
             });
-
-            AppHost = builder.Build();
+            return builder;
         }
+
+        private static IConfigurationRoot BuildAppConfigurationRoot(string environmentName)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+                .Build();
+            return configuration;
+        }
+
+        private static void CreateLoggerSerilog(IConfigurationRoot configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+                
+        }
+
+
+
         protected override void OnStartup(StartupEventArgs e)
         {
             AppHost!.StartAsync();
